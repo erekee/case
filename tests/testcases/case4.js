3fa1f5c858402eb7299ee284e945e822595ad7c8
erekee <43266120+erekee@users.noreply.github.com>
2019-03-16 09:06:56 +0600

    Update case4.js

--- tests/testcases/case4.js
+++ tests/testcases/case4.js
@@ -5,7 +5,7 @@ browser
 .url('https://store.steampowered.com/login/?redir=&redir_ssl=1')
 .waitForElementVisible('body', 10000)
 .click('#link_forgot_password')
-.setValue('#link_forgot_password > div > p:nth-child(2) > input[type="email"]', 'tungpr123@mail.ru')
+.setValue('#link_forgot_password > div > p:nth-child(2) > input[type="email"]', 'mr.ernur99@mail.ru')
 .click('#link_forgot_password > div > p:nth-child(3) > input')
 .pause(1000)
 .end();
